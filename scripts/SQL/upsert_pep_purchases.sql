a49159215ff83cb93f3b1b6ec5e65e31ae7816ae
ijochen <80488484+ijochen@users.noreply.github.com>
2021-05-19 14:16:45 -0700

    edit upsert_pep_purchases stored proc

--- scripts/SQL/upsert_pep_purchases.sql
+++ scripts/SQL/upsert_pep_purchases.sql
@@ -1,17 +1,21 @@
+--DROP FUNCTION procurement.upsert_pep_purchases()
+
 CREATE FUNCTION procurement.upsert_pep_purchases() RETURNS void AS $$
 BEGIN
 
     --Delete
     delete from procurement.pep_purchase_orders
-    where PO_NUM in (
-        select PO_NUM
-        from procurement.pep_purchase_order_incremental
+    where "PO_NUM" in (
+        select "PO_NUM"
+        from procurement.pep_purchase_orders_incremental
     );
 
     --Reinsert
     insert into procurement.pep_purchase_orders
-    select * from procurement.pep_purchase_order_incremental;
+    select * from procurement.pep_purchase_orders_incremental;
 
 END;
 $$
 LANGUAGE plpgsql;
+
+--SELECT procurement.upsert_pep_purchases();
