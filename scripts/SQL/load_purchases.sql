6c921449395a6848768769203dbd07511a7d7d4b
ijochen <80488484+ijochen@users.noreply.github.com>
2021-07-26 14:32:08 -0700

    Update FWEP Purchase cube to write to Aquacentral Schema Public DW

--- scripts/SQL/load_purchases.sql
+++ scripts/SQL/load_purchases.sql
@@ -11,6 +11,16 @@ BEGIN
     UNION
         (SELECT DISTINCT * FROM procurement.pep_purchase_orders)
     ) a;
+
+    DROP TABLE IF EXISTS aquacentral.fwep_purchase_data_merged;
+
+    CREATE TABLE aquacentral.fwep_purchase_data_merged AS
+    SELECT DISTINCT * 
+    from (
+        (SELECT DISTINCT * FROM procurement.fwp_purchase_orders)
+    UNION
+        (SELECT DISTINCT * FROM procurement.pep_purchase_orders)
+    ) a;
 END;
 $$
 LANGUAGE plpgsql;
