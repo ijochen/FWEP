fee504810fac424290bb74d6f850e3f95ab22a73
ijochen <80488484+ijochen@users.noreply.github.com>
2021-08-09 21:20:49 -0700

    renamed fwep purchases stored procedure and now writing to aquacentral

--- scripts/SQL/load_fwep_purchases.sql
+++ scripts/SQL/load_fwep_purchases.sql
@@ -1,6 +1,6 @@
 --DROP FUNCTION procurement.load_purchases()
 
-CREATE FUNCTION procurement.load_purchases() RETURNS void AS $$
+CREATE FUNCTION procurement.load_fwep_purchases() RETURNS void AS $$
 BEGIN   
     DROP TABLE IF EXISTS procurement.fwep_purchase_data_merged;
 
@@ -20,7 +20,7 @@ BEGIN
         (SELECT DISTINCT * FROM procurement.fwp_purchase_orders)
     UNION
         (SELECT DISTINCT * FROM procurement.pep_purchase_orders)
-    ) a;
+    ) b;
 END;
 $$
 LANGUAGE plpgsql;
