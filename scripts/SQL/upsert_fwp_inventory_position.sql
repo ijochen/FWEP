ed0e79fd1e813ad8d206937b4a53e381d8c62fef
ijochen <80488484+ijochen@users.noreply.github.com>
2021-08-10 17:51:59 -0700

    don't select the date because it messes up inserting unique values because it sees different dates

--- scripts/SQL/upsert_fwp_inventory_position.sql
+++ scripts/SQL/upsert_fwp_inventory_position.sql
@@ -3,10 +3,12 @@ begin
 	
 	--Insert item in locations where quantities has changed so we can see the trends 
 	insert into warehouse.inventory_position
-    select * from (
-        select * from warehouse.inventory_position_incremental
+    select *, current_date trans_date from (
+        select ipi.location_id, ipi.branch_description, ipi.item_group, ipi.item_id, ipi.item_desc, ipi.avg_fifo_cost, ipi.qty_on_hand, ipi.qty_allocated, ipi.qty_available
+        from warehouse.inventory_position_incremental ipi
         except
-        select * from warehouse.inventory_position
+        select ip.location_id, ip.branch_description, ip.item_group, ip.item_id, ip.item_desc, ip.avg_fifo_cost, ip.qty_on_hand, ip.qty_allocated, ip.qty_available 
+        from warehouse.inventory_position ip
         ) t1;
 	
 END ;
