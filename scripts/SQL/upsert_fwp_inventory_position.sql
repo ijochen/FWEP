15739a6e6708febb4491e3aa22caf79204e7f08e
ijochen <80488484+ijochen@users.noreply.github.com>
2021-08-12 17:35:07 -0700

    Added comments on each section of code logic

--- scripts/SQL/upsert_fwp_inventory_position.sql
+++ scripts/SQL/upsert_fwp_inventory_position.sql
@@ -9,8 +9,10 @@ BEGIN
     CREATE TABLE warehouse.inventory_position_count as 
     SELECT *, COUNT(item_id) OVER( PARTITION BY location_id, item_id, qty_on_hand, qty_allocated, qty_available) COUNT
     FROM (
+        --original table from first ETL
         SELECT * FROM warehouse.inventory_position_interim
         UNION
+        --daily refreshed table of what's currently in stock at each branch
         SELECT *, CURRENT_DATE trans_date FROM warehouse.inventory_position_incremental
     ) t1;
 
@@ -24,15 +26,19 @@ BEGIN
         SELECT * FROM warehouse.inventory_position_count
     ) t2;
 
+    --delete the duplicates before inserting it into the production table
     DELETE FROM warehouse.inventory_position_rank 
     WHERE count = 2 and rank = 2;
 
+    --delete all rows before inserting the unique/distinct records
     TRUNCATE TABLE warehouse.inventory_position;
 
+    --reinsert distinct records
     INSERT INTO warehouse.inventory_position
     SELECT location_id, branch_description, item_group, item_id, item_desc, avg_fifo_cost, qty_on_hand, qty_allocated, qty_available, trans_date
     FROM warehouse.inventory_position_rank;
 
+    --drop these tables to save on cost/space
     DROP TABLE IF EXISTS warehouse.inventory_position_count, warehouse.inventory_position_rank ;
 
 	
