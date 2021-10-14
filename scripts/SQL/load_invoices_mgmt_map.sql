7cc42c4307f42dc171ee5044ea3227200f30b96f
ijochen <80488484+ijochen@users.noreply.github.com>
2021-10-14 14:01:54 -0700

    Change the join to remove leading zero from branch_id instead of having to add the leading zeros to the location ID from the mapping sheet

--- scripts/SQL/load_invoices_mgmt_map.sql
+++ scripts/SQL/load_invoices_mgmt_map.sql
@@ -7,26 +7,29 @@ BEGIN
     CREATE TABLE sales.invoice_data_merged_mgmt AS
     SELECT * FROM (
         select 
-        order_no,
-        invoice_no,
-        invoice_date,
-        branch,
-        order_date,
-        total_sales, 
-        total_cost,
-        channel,
-        company,
-        "Director" director,
-        "State" state,
-        "Region Detail" region_detail,
-        "Regional Leader" regional_leader,
-        "City" city,
-        trim(leading '0' from "Location ID") location_id, 
-        "Region" region,
-        "Manager" manager,
-        "Time Zone" time_zone       
+            order_no,
+            invoice_no,
+            invoice_date,
+            branch,
+            order_date,
+            total_sales, 
+            total_cost,
+            channel,
+            company,
+            "Director" director,
+            "State" state,
+            "Region Detail" region_detail,
+            "Regional Leader" regional_leader,
+            "City" city,
+            --lpad("Location ID"::text,3,'0'),
+            --trim(leading '0' from "Location ID") location_id, 
+            trim(leading '0' from branch_id) location_id, 
+            "Region" region,
+            "Manager" manager,
+            "Time Zone" time_zone       
         from sales.invoice_data_merged sidm
-        left join warehouse.regional_mgmt_mapping_data rmmd on sidm.branch = rmmd."City" and sidm.branch_id = rmmd."Location ID"
+        left join warehouse.regional_mgmt_mapping_data rmmd on sidm.branch = rmmd."City" and trim(leading '0' from branch_id) = rmmd."Location ID"
+        -- and sidm.branch_id = lpad(rmmd."Location ID"::text,3,'0')
     ) a;
 
 END;
