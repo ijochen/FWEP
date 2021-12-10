291cb4a82f5627573c5d3298107870bdfa965229
ijochen <80488484+ijochen@users.noreply.github.com>
2021-12-10 13:16:45 -0800

    regional mgmt Location ID column saved as general data type so that the join works

--- scripts/SQL/load_invoices_mgmt_map.sql
+++ scripts/SQL/load_invoices_mgmt_map.sql
@@ -23,15 +23,16 @@ BEGIN
             "City" city,
             --lpad("Location ID"::text,3,'0'),
             --trim(leading '0' from "Location ID") location_id, 
-            trim(leading '0' from branch_id) location_id, 
+            trim(leading '0' from branch_id) location_id, --Marty wants to remove the leading zeroes everywhere 
             "Region" region,
             "Manager" manager,
             "Time Zone" time_zone,
             "Buyer" buyer,
             "Former Entity" former_entity       
         from sales.invoice_data_merged sidm
-        left join warehouse.regional_mgmt_mapping_data rmmd on sidm.branch = rmmd."City" and branch_id = rmmd."Location ID"
-        --left join warehouse.regional_mgmt_mapping_data rmmd on sidm.branch = rmmd."City" and trim(leading '0' from branch_id) = rmmd."Location ID"
+        --use this join and save the "Location ID" column in the regional mgmt mapping sheet in sharepoint as a general data type so the join doesn't duplicate and mismatch P21 and Prelude's branch ID leading zeroes 
+        left join warehouse.regional_mgmt_mapping_data rmmd on sidm.branch = rmmd."City" and trim(leading '0' from branch_id) = rmmd."Location ID"
+        -- left join warehouse.regional_mgmt_mapping_data rmmd on sidm.branch = rmmd."City" and branch_id = rmmd."Location ID"
         -- and sidm.branch_id = lpad(rmmd."Location ID"::text,3,'0')
     ) a;
 
