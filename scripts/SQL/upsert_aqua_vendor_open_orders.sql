c76c78c10486df353fc53b041d5adb2a4324d95a
ijochen <80488484+ijochen@users.noreply.github.com>
2021-12-29 12:34:08 -0800

    updated reinsert section to insert into aqua vendor oo instead of fwp invoices

--- scripts/SQL/upsert_aqua_vendor_open_orders.sql
+++ scripts/SQL/upsert_aqua_vendor_open_orders.sql
@@ -1,6 +1,6 @@
 
 -- save the interim as the original file, incase we need to revert back, use the pristine table as the table for Tableau
--- SELECT * INTO procurement.vendor_open_orders
+-- SELECT * INTO procurement.aqua_vendor_open_orders
 -- FROM procurement.vendor_open_orders_interim
 
 -- DROP FUNCTION procurement.upsert_aqua_vendor_open_orders()
@@ -9,15 +9,15 @@ CREATE FUNCTION procurement.upsert_aqua_vendor_open_orders()  RETURNS void AS $$
 BEGIN
 	
 	--delete past vendor open orders data because we only want to see the latest updates
-    DELETE FROM procurement.vendor_open_orders 
+    DELETE FROM procurement.aqua_vendor_open_orders 
     WHERE vendor IN (
         SELECT vendor 
-        FROM procurement.vendor_open_orders_incremental 
+        FROM procurement.aqua_vendor_open_orders_incremental 
     );
 	
 	-- Reinsert 
-	INSERT INTO sales.fwp_invoice_data 
-	SELECT * FROM sales.fwp_invoice_data_incremental;
+	INSERT INTO procurement.aqua_vendor_open_orders  
+	SELECT * FROM procurement.aqua_vendor_open_orders_incremental;
 	
 END ;
 $$
